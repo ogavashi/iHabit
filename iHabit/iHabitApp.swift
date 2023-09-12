fa545e63ce960f9ef84f38090c83b088fc22aa57
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2023-09-12 15:43:13 +0300

    Initial Commit

---
+++ iHabit/iHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  iHabitApp.swift
+//  iHabit
+//
+//  Created by Oleg Gavashi on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct iHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
